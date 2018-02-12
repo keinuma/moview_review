dc32c3721298f9a6e0f654666323d0341b092613
numata <nununu.mono@gmail.com>
2018-02-12 19:04:28 +0900

    GCOからデータのロード-解析まで実装
    TODO
    - 解析結果のまとめ方について調査
    - データの量を増やして実行

--- pymovie/analysis/gcloud_nltk.py
+++ pymovie/analysis/gcloud_nltk.py
@@ -9,13 +9,25 @@ from google.cloud import translate
 from google.cloud import language
 
 
-def translate_review(client, jp_text):
+def load_data(query):
+    """
+    BigQueryからデータをロードする
+    :param str query: 発行するクエリ
+    :return:
+    """
+    client = bigquery.Client()
+    job = client.query(query=query)
+    data = job.result()
+    return data
+
+
+def translate_review(jp_text):
     """
     GCPから映画レビューの翻訳を行う
-    :param google.cloud client: APIクライアントオブジェクト
     :param str jp_text: 日本語レビュー文章
     :return str en_content: 英語に翻訳後の文章
     """
+    client = translate.Client()
     translation = client.translate(
         jp_text,
         target_language="en"
@@ -23,13 +35,13 @@ def translate_review(client, jp_text):
     return translation["translatedText"]
 
 
-def analysis_review(client, content):
+def analysis_review(content):
     """
     英語の映画レビューの感情分析を行う
-    :param google.cloud client: APIクライアントオブジェクト
     :param str content: 英語レビュー文章
-    :return dict result: 感情分析結果(JSON)
+    :return list sentiment: 感情分析結果(JSON)
     """
+    client = language.LanguageServiceClient()
     document = language.types.Document(
         content=content,
         language="en",
@@ -40,7 +52,8 @@ def analysis_review(client, content):
         encoding_type="UTF32"
     )
     sentiments = [
-        (x["name"], x["sentiment"]) for x in response
+        (x.name, x.mentions[0].sentiment) for x in response.entities
+        if x.mentions[0].sentiment.score != 0.
     ]
     return sentiments
 
@@ -50,5 +63,17 @@ def main():
     メイン処理
     :return:
     """
-    trans_client = translate.Client()
-    analyze_client = language.LanguageServiceClient()
+    query = """
+    select code, content from moviereview.review limit 10
+    """
+    data = load_data(query)
+    result = []
+    for _, content in data:
+        en_text = translate_review(content)
+        analyzed = analysis_review(en_text)
+        result.append(analyzed)
+    return result
+
+
+if __name__ == "__main__":
+    main()
