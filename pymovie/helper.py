8efdc5ef8167733bd3d81d1c7602832525a2b00b
numata <nununu.mono@gmail.com>
2018-03-08 14:06:56 +0900

    データベースモデル定義

--- pymovie/helper.py
+++ pymovie/helper.py
@@ -3,6 +3,9 @@
 ヘルパー関数定義
 """
 
+from pymongo import MongoClient
+from .config import MONGO
+
 
 def init_getter(session, column):
     """
@@ -13,3 +16,24 @@ def init_getter(session, column):
     """
     data = (x[0] for x in session.quey(column).all())
     return data
+
+
+def mongo_conn(collection):
+    """
+    MongoDBから指定のコレクションを取得する
+    :param str collection: MongDBのコレクション文字列
+    :return:
+    """
+    params = {
+        'host': MONGO['HOST'],
+        'port': MONGO['PORT']
+    }
+    client = MongoClient(**params)
+    db = client[MONGO['DB']]
+    if collection == 'movie':
+        col = db[MONGO['COLLECTION_M']]
+    elif collection == 'review':
+        col = db[MONGO['COLLECTION_R']]
+    else:
+        raise Exception('Not found mongo collection')
+    return col
