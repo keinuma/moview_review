de16169fb055711a832c3cc54ab242ea0a4ef7a1
numata <nununu.mono@gmail.com>
2018-03-10 20:47:03 +0900

    reviewクローリング修正

---
+++ pymovie/crawlmovie/models.py
@@ -0,0 +1,92 @@
+"""
+2018/01/21 numata
+データベースのORM
+"""
+
+from datetime import datetime
+from sqlalchemy import Column, Integer, Unicode, DateTime, Float
+from .settings import BASE
+
+
+class Movie(BASE):
+    """
+    映画モデル:
+        code: int - 主キー
+        title: string - 映画タイトル
+        open_date: datetime - 公開日
+        created: datetime - 作成日
+    """
+    __tablename__ = 'movie_movie'
+    movie_code = Column(Integer, primary_key=True)
+    title = Column(Unicode(50))
+    open_date = Column(Unicode(20))
+    description = Column(Unicode(300))
+    director = Column(Unicode(30))
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    def __init__(self, movie_code, title=None, open_date=None,
+                 description=None, director=None):
+        self.movie_code = movie_code
+        self.title = title
+        self.open_date = open_date
+        self.description = description
+        self.director = director
+        self.created_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    def __repr___(self):
+        return '<Movie: %r>' % self.title
+
+
+class Review(BASE):
+    """
+    レビューモデル:
+        code: int - 主キー
+        movie_code: int - 映画モデルのキー
+        points: int - 映画の評価点
+        content: sting - 映画レビュー
+        empathy: string - レビューの共感度
+        created: datetime - 作成日
+    """
+    __tablename__ = 'movie_review'
+    review_code = Column(Integer, primary_key=True)
+    movie_code = Column(Integer)
+    points = Column(Integer)
+    content = Column(Unicode)
+    empathy = Column(Integer)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    def __init__(self, code, movie_code=None, points=None,
+                 content=None, empathy=None):
+        self.code = code
+        self.movie_code = movie_code
+        self.points = points
+        self.content = content
+        self.empathy = empathy
+        self.created = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    def __repr__(self):
+        return '<Review: %r>' % self.content
+
+
+class Analyzed(BASE):
+    """
+    レビューモデル:
+        code: int - 主キー
+        magnitude: float - 感情の振れ幅
+        score: float - 感情
+    """
+    __tablename__ = 'movie_analyzed'
+    review_code = Column(Integer, primary_key=True)
+    magnitude = Column(Float)
+    score = Column(Float)
+
+    def __init__(self, code, magnitude=None, score=None):
+        self.code = code
+        self.magnitude = magnitude
+        self.score = score
+
+    def __repr__(self):
+        return '<Score: %r, Magnitude: %r>' % (self.score, self.magnitude)
--- pymovie/crawlmovie/spiders/reviews.py
+++ pymovie/crawlmovie/spiders/reviews.py
@@ -5,10 +5,28 @@ reviewページのスクレイピング
 
 import re
 from scrapy import Spider, Request
-from pymongo import MongoClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-from ..settings import HOST, PORT, DB, COLLECTION_MOVIE
+from ..settings import DATABASE
+from ..models import Movie
 from ..items import ReviewPage
+
+
+ENGINE = create_engine(
+    DATABASE,
+    encoding='utf8',
+    echo=True
+)
+
+# session作成
+SESSION = scoped_session(
+    sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=ENGINE
+    )
+)
 
 
 class ReviewsSpider(Spider):
@@ -18,16 +36,10 @@ class ReviewsSpider(Spider):
     review_url = 'http://eiga.com/movie/{}/review/all/'
 
     def __init__(self):
-        params = {
-            'host': HOST,
-            'port': PORT
-        }
-        client = MongoClient(**params)
-        db = client[DB]
-        collection = db[COLLECTION_MOVIE]
-        self.movie_code = collection.find(
-            {},
-            {'code': 1, '_id': 0}
+        session = SESSION()
+        self.movie_code = (
+            str(x[0]) for x
+            in session.query(Movie.movie_code).all()
         )
 
     def parse(self, response):
@@ -37,7 +49,7 @@ class ReviewsSpider(Spider):
         :return:
         """
         for code in self.movie_code:
-            url = self.review_url.format(code['code'])
+            url = self.review_url.format(str(code))
             yield Request(url, self.parse_review)
 
     def parse_review(self, response):
@@ -59,7 +71,7 @@ class ReviewsSpider(Spider):
         :return:
         """
         for review in response.css('div.review'):
-            if "本文にネタバレがあります。" in review.css("p > em::text").extract_first():
+            if review.css("p > em::text").extract_first():
                 continue
             item = ReviewPage()
             item['code'] = review.css('h3 > a::attr(href)').re('\d+')[1]
